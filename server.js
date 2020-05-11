9564f66e34b0a8345a0ea311d88348f25b603a55
DehNutCase <dslayerz@hotmail.com>
2020-05-11 08:24:07 -0700

    Firebase authentication with email added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("express")();
 let http = require('http').Server(app);
 let fs = require('fs');
 const io = require("socket.io")(http);
+
 const port = 8000;
   app.get("/", function(req, res) {
         res.sendFile(__dirname + "/guessing_game.html");
